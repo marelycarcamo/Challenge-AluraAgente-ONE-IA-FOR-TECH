d54ebcb4dc3147422177bb4ca31f7c57c015be43
marely <marely.carcamo@gmail.com>
2026-07-19 22:55:56 -0400

    feat: improve Streamlit interface with branding and avatars

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -17,9 +17,25 @@ Descripción:
 Proyecto:
     ALESSIA
     Asistente basado en RAG para la gestión del riesgo de desastres.
+
+Punto de entrada de la aplicación Streamlit.
+
+Responsable de:
+- Configurar la interfaz principal.
+- Cargar los recursos visuales.
+- Inicializar el servicio de aplicación.
+- Renderizar la interfaz conversacional.
 """
+from pathlib import Path
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.append(str(PROJECT_ROOT))
 
 
+from app.bootstrap import load_agent_service
 import streamlit as st
 
 from app.bootstrap import load_agent_service
@@ -28,56 +44,50 @@ from app.config import (
     APP_DESCRIPTION,
     APP_ICON,
     APP_NAME,
+    # ALESSIA_AVATAR_PATH,
+    # USER_AVATAR_PATH,
     LOGO_PATH,
+    AVATAR_PATH
 )
 
+
+# --------------------------------------------------
+# Configuración de la aplicación
+# --------------------------------------------------
 
 st.set_page_config(
     page_title=APP_NAME,
     page_icon=APP_ICON,
-    layout="centered",
+    layout="wide",
 )
 
+# --------------------------------------------------
+# Barra lateral
+# --------------------------------------------------
 
-# -----------------------------
-# Identidad visual
-# -----------------------------
+with st.sidebar:
 
-if LOGO_PATH.exists():
-    st.image(
-        str(LOGO_PATH),
-        width=180,
-    )
+    if LOGO_PATH.exists():
+        st.image(
+            str(LOGO_PATH),
+            use_container_width=True,
+        )
+    
+    if AVATAR_PATH.exists():
+        st.image(
+            str(AVATAR_PATH),
+            use_container_width=True,
+        )   
 
-st.title(APP_NAME)
+# --------------------------------------------------
+# Inicializar servicio
+# --------------------------------------------------
 
-st.write(APP_DESCRIPTION)
+agent_service = load_agent_service()
 
-st.divider()
-
-
-# -----------------------------
-# Inicialización del servicio
-# -----------------------------
-
-@st.cache_resource
-def get_agent_service():
-    """
-    Carga el servicio principal de la aplicación.
-
-    Streamlit mantiene una única instancia en memoria para evitar
-    reconstruir los modelos del pipeline RAG en cada interacción.
-    """
-
-    return load_agent_service()
-
-
-agent_service = get_agent_service()
-
-
-# -----------------------------
+# --------------------------------------------------
 # Interfaz conversacional
-# -----------------------------
+# --------------------------------------------------
 
 render_chat(
     agent_service=agent_service,
